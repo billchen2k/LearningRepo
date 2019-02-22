97251f9180881c32b60e714ca436aca0fe9c04c8
Bill Chen <Bill.Chen@live.com>
2019-02-22 14:20:07 +0800

    uploaded the Game of Life

---
+++ Course/数据结构与算法/LifeGame_Clion/cmake-build-debug/CMakeFiles/LifeGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LifeGame.dir/main.cpp.obj"
+  "LifeGame.pdb"
+  "LifeGame.exe"
+  "LifeGame.exe.manifest"
+  "LifeGame.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LifeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
