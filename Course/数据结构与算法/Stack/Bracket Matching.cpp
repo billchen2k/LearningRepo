4aa2c4e8a7dfa93d5fb11a121410db62903ec6c4
Bill Chen <Bill.Chen@live.com>
2019-03-03 16:32:48 +0800

    updated bracket matching

--- Course/数据结构与算法/Stack/Bracket Matching.cpp
+++ Course/数据结构与算法/Stack/Bracket Matching.cpp
@@ -5,50 +5,42 @@ const int MAXL = 1000;
 using namespace std;
 char left_bracket[4] = {'(', '[', '{', '<'};
 char right_bracket[4] = {')', ']', '}', '>'};
-int is_subset(char src, char *dst)
-{
+int is_subset(char src, char *dst){
     int i;
-    for (i = 0; i < sizeof(src) / sizeof(char); i++)
-    {
+    for (i = 0; i < 4; i++){
         if (src == dst[i])
             return i + 1;
     }
     return 0;
 }
-void printer(int flag)
-{
+void printer(int flag){
     cout << (flag ? "Matched.\n" : "Not Matched.\n");
 }
-int main()
-{
+int main(){
     cout << "Please type one line including brackets(Eg. ()[]{}),\n and we weill verify if these brackets are correctly matched." << endl
          << endl
          << "input Q or q or an empty line to quit." << endl;
     char input[MAXL];
-    while (cin.getline(input, MAXL) && !cin.fail() && !((toupper(input[0]) == 'Q' && input[1] == '\0') || input[0] == '\0'))
-    {
+    while (cin.getline(input, MAXL) && !cin.fail() 
+            && !((toupper(input[0]) == 'Q' && input[1] == '\0') || input[0] == '\0')) {
         int flag = -1;
         stack<char> data;
-        for (int i = 0; i < strlen(input); i++)
-        {
-            if (is_subset(input[i], left_bracket))
-            {
+        for (int i = 0; i < strlen(input); i++){
+            if (is_subset(input[i], left_bracket)){
                 data.push(input[i]);
             }
-            if (is_subset(input[i], right_bracket))
-            {
-                if (!data.empty())
-                {
-                    if (!((is_subset(data.top(), left_bracket)) == (is_subset(input[i], right_bracket))))
-                    {
+            else if (is_subset(input[i], right_bracket)){
+                if (!data.empty()) {
+                    if ((is_subset(data.top(), left_bracket)) == (is_subset(input[i], right_bracket))) {
+                        flag = 1;
+                        data.pop();
+                    }
+                    else{
                         flag = 0;
                         break;
                     }
-                    else
-                        data.pop();
-                }
-                else
-                {
+             }
+                else{
                     flag = 0;
                     break;
                 }
@@ -56,7 +48,7 @@ int main()
         }
         if (!data.empty())
             flag = 0;
-        else if (data.empty() && flag == -1)
+        if (data.empty() && flag == -1)
             flag = 1;
         printer(flag);
     }
