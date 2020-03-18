ca45ecba943b8763d769e29bf173cb5db1f2e965
Bill Chen <Bill.Chen@live.com>
2020-03-18 15:58:06 +0800

    AI Drug Assigenment 1 Finished

--- ACM/temp.cpp
+++ ACM/temp.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 int main() {
-	int n, m;
-	cin >> n >> m;
-	int* a = new int[n];
-
+	map<string, int> mymap;
+	mymap.insert(make_pair("abc", 2));
+	mymap.insert(make_pair("rcd", 3));
+	mymap["tester"] = 4;
+	mymap["tester"] = 5;
+	for (map<string, int>::iterator it = mymap.begin(); it != mymap.end(); it++) {
+		cout << it->first << ":" << it->second << endl;
+		cout << mymap.count("abc");
+	}
 }
