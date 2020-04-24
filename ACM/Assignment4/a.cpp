ebf1b4313276a3bac2c12d52a099973c24f1b4a8
Bill Chen <Bill.Chen@live.com>
2020-04-24 12:45:25 +0800

    ACM assignment 4

--- ACM/Assignment4/a.cpp
+++ ACM/Assignment4/a.cpp
@@ -1,22 +1,32 @@
 #include <algorithm>
 #include <iostream>
-
+#include <string.h>
 
 using namespace std;
 
-int number[101];
-int level[101];
+int dp[1005];
+int number[1005];
+int cost[1005];
+int sum[1005];
 int main() {
 	int n, t;
 	cin >> n;
-	while(n--){
+	while (n--) {
 		cin >> t;
-		for (int i = 0; i < t; i++){
-			int ta, tb;
-			cin >> ta >> tb;
-			number[i] = ta;
-			level[i] = tb;
+		memset(dp, 0, sizeof(0));
+		memset(sum, 0, sizeof(0));
+		for (int i = 1; i <= t; i++) {
+			cin >> number[i] >> cost[i];
+			sum[i] = sum[i - 1] + number[i];
 		}
+		dp[0] = 0;
+		for (int i = 1; i <= t; i++) {
+			dp[i] = dp[i - 1] + cost[i] * (number[i] + 10);
+			for (int j = 0; j < i;j++){
+				dp[i] = min(dp[i], dp[j] + (sum[i] - sum[j] + 10) * cost[i]);
+			}
+		}
+		cout << dp[t] << endl;
 	}
-	
+	return 0;
 }
